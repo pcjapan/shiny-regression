336fba5d4dafb77fb87a21779efdedf98875e9cd
pcjapan <collett@mac.com>
2021-09-08 16:34:22 +0900

    Regression Added

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 ## File upload functionality based on https://stackoverflow.com/a/36955396
 
 library(shiny)
+library(shinyWidgets)
 library(pastecs)
 library(ggplot2)
 
@@ -8,120 +9,217 @@ library(ggplot2)
 
 
 ui <- shinyUI(fluidPage(
-    titlePanel("Column Plot"),
-    tabsetPanel(
-        tabPanel("Upload File",
-                 titlePanel("Uploading Files"),
-                 sidebarLayout(
-                     sidebarPanel(
-                         fileInput('file1', 'Choose CSV File',
-                                   accept=c('text/csv', 
-                                            'text/comma-separated-values,text/plain', 
-                                            '.csv')),
-                         
-                         # added interface for uploading data from
-                         # http://shiny.rstudio.com/gallery/file-upload.html
-                         tags$br(),
-                         checkboxInput('header', 'Header', TRUE),
-                         radioButtons('sep', 'Separator',
-                                      c(Comma=',',
-                                        Semicolon=';',
-                                        Tab='\t'),
-                                      ','),
-                         radioButtons('quote', 'Quote',
-                                      c(None='',
-                                        'Double Quote'='"',
-                                        'Single Quote'="'"),
-                                      '"'),
-                         # Input boxes for graph labels and title
-                         textInput("XVar", "X-Variable Label", value = "X", width = "400px", placeholder = "Enter a label for the X Axis"),
-                         textInput("YVar", "Y-Variable Label", value = "Y", width = "400px", placeholder = "Enter a label for the Y Axis"),
-                         textInput("pTitle", "Plot Title", value = "My Scatterplot", width = "400px", placeholder = "Enter a descriptive title")
-                     ),
-                     mainPanel(
-                         tableOutput('contents')
-                     )
-                 )
-        ),
+  
+  tags$head(
+    
+    # styling output
+    
+    tags$style(
+      "
       
-        
-        tabPanel("Output",
-                 pageWithSidebar(
-                     headerPanel('Analysis Output'),
-                     sidebarPanel(
-                         
-                         # "Empty inputs" - they will be updated after the data is uploaded
-                         selectInput('xcol', 'X Variable', ""),
-                         selectInput('ycol', 'Y Variable', "", selected = "")
-                         
-                     ),
-                     mainPanel(
-                         plotOutput('MyPlot'),
-                         verbatimTextOutput('StatSum')
-                     )
-                 )
-        )
-        
+      textarea {
+        padding:10px;
+        min-width: 20px; !important;
+        position: relative;
+      }
+      
+      p { line-height: 1.5; }
+      
+      h3 {
+      line-height: 2;
+      color: red;
+      border-bottom: 2px solid;
+      }
+      
+      pre#StatSum { width: 400px; }
+			
+      pre#Regress { width: 600px; }
+      
+      .instructText { 
+      font-style: italic;
+      line-height:2;
+      margin-bottom: 6pt;
+      } 
+      "
+
     )
-)
-)
+  ),
+  
+  titlePanel("Regression Analysis"),
+  tabsetPanel(
+    tabPanel(
+      "Upload File",
+      titlePanel("Uploading Files"),
+      sidebarLayout(
+        sidebarPanel(
+          fileInput(
+            'file1',
+            'Choose CSV File',
+            accept = c('text/csv',
+                       'text/comma-separated-values,text/plain',
+                       '.csv')
+          ),
+          
+          # added interface for uploading data from
+          # http://shiny.rstudio.com/gallery/file-upload.html
+          p("The variables below control how the application reads your data. Please set as required.", class = "lead"),
+          tags$div("If your data table has a header row, make sure the checkbox below is checked.", class = "instructText"),
+          awesomeCheckbox('header', 'Header', TRUE, status = "success"),
+          tags$div("How are the columns separated? Select the correct option.", class = "instructText"),
+          awesomeRadio('sep', 'Separator',
+                       c(
+                         Comma = ',',
+                         Semicolon = ';',
+                         Tab = '\t'
+                       ),
+                       ',',
+                       status = "success"),
+          tags$div("Are the table values quoted (e.g.; \"1\",\"2\",...)? Indicate below.", class = "instructText"),
+          awesomeRadio(
+            'quote',
+            'Quote',
+            c(
+              None = '',
+              'Double Quote' = '"',
+              'Single Quote' = "'"
+            ),
+            '"',
+            status = "success"
+          )
+        ),
+        mainPanel(tableOutput('contents'))
+      )
+    ),
+    
+    
+    tabPanel(
+      "Output",
+      pageWithSidebar(
+        headerPanel('Analysis Output'),
+        sidebarPanel(
+          p("Select the x and y values from your data."),
+          # "Empty inputs" - they will be updated after the data is uploaded
+          selectInput('xcol', 'X Variable', "", width = "400px"),
+          selectInput('ycol', 'Y Variable', "", selected = "", width = "400px"),
+          
+          # Input boxes for graph labels and title
+          tags$br(),
+          p(
+            "Enter meaningful values for the X and Y axes, along with a title for the graph."
+          ),
+          textInput("XVar", "X Axis Label", value = "X", width = "400px"),
+          textInput("YVar", "Y Axis Label", value = "Y", width = "400px"),
+          textInput(
+            "pTitle",
+            "Plot Title",
+            value = "My Scatterplot",
+            width = "400px",
+            placeholder = "Enter a descriptive title"
+          )
+        ),
+        mainPanel(
+          tags$h3("Descriptive Statistics"),
+          verbatimTextOutput("StatSum"),
+          tags$br(),
+          tags$h3("Regression Analysis"),
+          verbatimTextOutput("Regress"),
+          tags$br(),
+          tags$h3("Scatterplot"),
+          plotOutput('MyPlot')
+          
+        )
+      )
+    )
+    
+  )
+))
 
 server <- shinyServer(function(input, output, session) {
+  # Read in the data
+  
+  data <- reactive({
+    req(input$file1)
+    
+    inFile <- input$file1
+    
+    df <-
+      read.csv(
+        inFile$datapath,
+        header = input$header,
+        sep = input$sep,
+        quote = input$quote
+      )
 
     
-data <- reactive({ 
-        req(input$file1)
-        
-        inFile <- input$file1 
-        
-        df <- read.csv(inFile$datapath, header = input$header, sep = input$sep,
-                       quote = input$quote)
-        
-        
-        
-        updateSelectInput(session, inputId = 'xcol', label = 'X Variable',
-                          choices = names(df), selected = names(df))
-        updateSelectInput(session, inputId = 'ycol', label = 'Y Variable',
-                          choices = names(df), selected = names(df)[2])
-        
-        return(df)
-    })
+    updateSelectInput(
+      session,
+      inputId = 'xcol',
+      label = 'X Variable',
+      choices = names(df),
+      selected = names(df)
+    )
+    updateSelectInput(
+      session,
+      inputId = 'ycol',
+      label = 'Y Variable',
+      choices = names(df),
+      selected = names(df)[2]
+    )
     
-    output$contents <- renderTable({
-        data()
-    })
+    return(df)
+  })
+
+  output$contents <- renderTable({
+    data()
+  })
+  
+# Plot
+
+  output$MyPlot <- renderPlot({
+    x <- data()[, (input$xcol)]
+    y <- data()[, (input$ycol)]
+    df2 <- data.frame(x = x,
+                      y = y)
+    ggplot(df2) +
+      aes(x = x, y = y) +
+      geom_point(size = 3, alpha = .8) +
+      geom_smooth(method = "lm", alpha = 0.1) +
+      theme_minimal() +
+      theme(
+        axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title = element_text(size = 16, lineheight = 2),
+        plot.title = element_text(
+          size = 16,
+          lineheight = 2,
+          face = "bold"
+        )
+      ) +
+      xlab(req(input$XVar)) +
+      ylab(req(input$YVar)) +
+      ggtitle(req(input$pTitle))
     
-    output$valueX <- renderText({ input$XVar })
-    
-    output$MyPlot <- renderPlot({
-        x <- data()[, (input$xcol)]
-        y <- data()[, (input$ycol)]
-        df2 <- data.frame(x = x,
-                         y = y)
-        ggplot(df2) +
-        aes(x = x, y = y) +
-            geom_point(size = 3, alpha = .8) +
-            geom_smooth(method = "lm", aes(linetype = "dotted"), alpha = 0.1) +
-            theme_minimal() +
-            theme(axis.text.x = element_text(size=14),
-                  axis.text.y = element_text(size = 14),
-                  axis.title = element_text(size = 16, lineheight = 2),
-                  plot.title = element_text(size = 14, lineheight = 2, face="bold"),
-                  strip.text.x = element_text(size = 14)) +
-          xlab(req(input$XVar)) +
-          ylab(req(input$YVar)) +
-          ggtitle(req(input$pTitle))
-        
-    })
-    
-    output$StatSum <- renderPrint({
-        x <- data()[, c(input$xcol, input$ycol)]
-        round(stat.desc(
-            x,
-            basic = TRUE,
-            norm = TRUE
-        ), digits = 3)
-    })
+  })
+  
+# Descriptive statistics
+  
+  output$StatSum <- renderPrint({
+    x <- data()[, c(input$xcol, input$ycol)]
+    round(stat.desc(x,
+                    basic = TRUE,
+                    norm = TRUE), digits = 3)
+  })
+  
+#  Regression
+
+  output$Regress <- renderPrint({
+    x <- data()[, (input$xcol)]
+    y <- data()[, (input$ycol)]
+    df3 <- data.frame(x = x,
+                      y = y)
+    rg <- lm(y ~ x, df3)
+    summary(rg)
+  })
 })
 
 shinyApp(ui, server)
